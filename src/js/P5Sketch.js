fa038e9c0c96cadbdebb70b03e8117da41159008
LABCAT <shane.watters.nz@gmail.com>
2021-01-16 17:40:51 +1300

    Adding canvas variable.

--- src/js/P5Sketch.js
+++ src/js/P5Sketch.js
@@ -4,12 +4,14 @@ import * as p5 from "p5";
 const P5Sketch = () => {
     const Sketch = p => {
 
+        p5.canvas = null;
+
         p.canvasWidth = window.innerWidth;
 
         p.canvasHeight = window.innerHeight;
 
         p.setup = () => {
-            p.createCanvas(p.canvasWidth, p.canvasHeight);
+            p5.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
         };
 
         p.draw = () => {
