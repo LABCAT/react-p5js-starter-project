bde7397c8d2d993ddd7b05b0f5868c268e861064
LABCAT <shane.watters.nz@gmail.com>
2021-09-24 10:21:19 +1200

    Replacing Tone.js with p5.sound as the performance seems to be much better.

--- src/js/P5SketchWithAudio.js
+++ src/js/P5SketchWithAudio.js
@@ -1,13 +1,13 @@
 import React, { useRef, useEffect } from "react";
+import "./helpers/Globals";
+import "p5/lib/addons/p5.sound";
 import * as p5 from "p5";
-import Tone from 'tone'
 import { Midi } from '@tonejs/midi'
 
-import audio from "../audio/rectangles-no-3.ogg";
-import midi from "../audio/rectangles-no-3.mid";
+import audio from "../audio/circles-no-3.ogg";
+import midi from "../audio/circles-no-3.mid";
 
 const P5SketchWithAudio = () => {
-    Tone.Transport.PPQ = 3840 * 4;
     const sketchRef = useRef();
 
     const Sketch = p => {
@@ -22,57 +22,68 @@ const P5SketchWithAudio = () => {
 
         p.player = null;
 
-        p.preload = () => {
-             Midi.fromUrl(midi).then(
+        p.PPQ = 3840 * 4;
+
+        p.loadMidi = () => {
+            Midi.fromUrl(midi).then(
                 function(result) {
-                    console.log(result.tracks);
-                    const noteSet1 = result.tracks[3].notes; // Sampler 1 - Heavy guitar
-                    p.player = new Tone.Player(audio, () => { p.audioLoaded = true; }).toMaster();
-                    p.player.sync().start(0);
-                    let lastTicks = -1;
-                    for (let i = 0; i < noteSet1.length; i++) {
-                        const note = noteSet1[i],
-                            { ticks, time } = note;
-                        if(ticks !== lastTicks){
-                            Tone.Transport.schedule(
-                                () => {
-                                    p.executeCueSet1(note);
-                                }, 
-                                time
-                            );
-                            lastTicks = ticks;
-                        }
-                    } 
+                    const noteSet1 = result.tracks[5].notes; // Synth 1
+                    p.scheduleCueSet(noteSet1, 'executeCueSet1');
+                    p.audioLoaded = true;
                 }
             );
+            
         }
+
+        p.preload = () => {
+            p.song = p.loadSound(audio, p.loadMidi);
+        }
+
+        p.scheduleCueSet = (noteSet, callbackName)  => {
+            let lastTicks = -1;
+            for (let i = 0; i < noteSet.length; i++) {
+                const note = noteSet[i],
+                    { ticks, time } = note;
+                if(ticks !== lastTicks){
+                    note.currentCue = i + 1;
+                    p.song.addCue(time, p[callbackName], note);
+                    lastTicks = ticks;
+                }
+            }
+        } 
 
         p.setup = () => {
             p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
             p.background(0);
-        };
+        }
 
         p.draw = () => {
-            
-        };
+            if(p.audioLoaded && p.song.isPlaying()){
+
+            }
+        }
 
         p.executeCueSet1 = (note) => {
             p.background(p.random(255), p.random(255), p.random(255));
             p.fill(p.random(255), p.random(255), p.random(255));
             p.noStroke();
             p.ellipse(p.width / 2, p.height / 2, p.width / 4, p.width / 4);
-        };
+        }
 
         p.mousePressed = () => {
             if(p.audioLoaded){
-                 if (p.player.state === "started") {
-                    Tone.Transport.pause(); // Use the Tone.Transport to pause audio
-                } 
-                else if (p.player.state === "stopped") {
-                    Tone.Transport.start(); // Use the Tone.Transport to start again
+                if (p.song.isPlaying()) {
+                    p.song.pause();
+                } else {
+                    if (parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)) {
+                        p.reset();
+                    }
+                    //document.getElementById("play-icon").classList.add("fade-out");
+                    p.canvas.addClass("fade-in");
+                    p.song.play();
                 }
             }
-        };
+        }
 
         p.updateCanvasDimensions = () => {
             p.canvasWidth = window.innerWidth;
